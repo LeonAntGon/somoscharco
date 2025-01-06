18a29acfe020ec4081cfe0a1c25c1aab4ba70402
unknown <gonzalezleonardoantonio056@gmail.com>
2025-01-06 13:12:04 -0300

    adding transitionPage

---
+++ app/_components/widgets/TransitionPage.tsx
@@ -0,0 +1,46 @@
+"use client"
+import { animate, AnimatePresence, motion} from "framer-motion"
+
+export function TransitionPage() {
+    return (
+        <AnimatePresence mode="wait">
+            <div>
+                <motion.div
+                className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-blue-900"
+                variants={transitionVariants}
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut"}}
+                >
+                </motion.div>
+                <motion.div
+                className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-blue-900/70 opacity-50"
+                variants={transitionVariants}
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                transition={{ delay: 0.4, duration: 0.6, ease: "easeInOut"}}
+                >
+
+                </motion.div>
+            </div>
+        </AnimatePresence>
+    )
+}
+
+const transitionVariants = {
+    initial:{
+        x:"100%",
+        width:"100%",
+    },
+    animate:{
+        x:"0%",
+        width:"0%"
+    },
+    exit: {
+        x: ["0%","100%"],
+        width:["0%","100%"]
+    }
+
+}
--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,12 @@ import Experience from "./_components/blocs/Experience";
 import Services from "./_components/blocs/Services";
 import Footer from "./_components/blocs/Footer";
 import  Contact from "@/app/_components/ui/Contact"
+import { TransitionPage } from "./_components/widgets/TransitionPage";
 
 export default function Home() {
   return (
     <>
+    <TransitionPage/>
     <Header/>
     <HeroSection/>
     <Somos/>
